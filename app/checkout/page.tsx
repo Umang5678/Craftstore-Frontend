790d4504355702910a9f64eeb0d1f93458fc284a
Umang Sheladiya <umang0sheladiya@gmail.com>
2025-12-16 11:42:02 +0530

    email to localstorage

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -523,6 +523,13 @@ export default function CheckoutPage() {
       items: cart,
       total,
     };
+    localStorage.setItem("userEmail", email);
+    // try {
+    //   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/orders`, {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(orderData),
+    //   });
 
     const res = await loadRazorpayScript();
     if (!res) {
